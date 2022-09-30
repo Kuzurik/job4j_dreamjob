3e283d291266cfcac551ca5daff7d9286d85749e
Lenovo <vorota-24@bk.ru>
2022-10-01 00:00:44 +0300

    8. HTML form. Создание кандидата. [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,7 +3,11 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.model.CandidateStore;
+import ru.job4j.dreamjob.model.Post;
+
+import java.util.Date;
 
 @Controller
 public class CandidateController {
@@ -14,5 +18,12 @@ public class CandidateController {
     public String postCandidates(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addPost(Model model) {
+        model.addAttribute("addCandidate",
+                new Candidate(0, "Новый кондидат", "Программист", new Date()));
+        return "addCandidate";
     }
 }
