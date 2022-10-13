60e1405fd789525d3b756020c917d85a281a5a4a
Lenovo <vorota-24@bk.ru>
2022-10-13 22:24:40 +0300

    5. Создание и редактирование кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/model/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/model/CandidateStore.java
@@ -1,23 +1,20 @@
 package ru.job4j.dreamjob.model;
 
+import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
     private static final CandidateStore INST = new CandidateStore();
 
+    private final AtomicInteger id = new AtomicInteger(0);
     private final Map<Integer, Candidate> candidateMap = new ConcurrentHashMap<>();
 
     private CandidateStore() {
-        candidateMap.put(1, new Candidate(1, "Max",
-                "Junior Java developer, experience one year", new Date()));
-        candidateMap.put(2, new Candidate(2, "Piter",
-                "Middle Java developer, experience two years", new Date()));
-        candidateMap.put(3, new Candidate(3, "Alex",
-                "Senior Java developer, experience five years", new Date()));
     }
 
     public static CandidateStore instOf() {
@@ -26,5 +23,20 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidateMap.values();
+    }
+
+    public void add(Candidate candidate) {
+        candidate.setId(id.getAndIncrement());
+        candidate.setCreated(new Timestamp(System.currentTimeMillis()));
+        candidateMap.put(candidate.getId(), candidate);
+    }
+
+    public void update(Candidate candidate) {
+        candidate.setCreated(new Timestamp(System.currentTimeMillis()));
+        candidateMap.replace(candidate.getId(), candidate);
+    }
+
+    public Candidate findById(int id) {
+        return candidateMap.get(id);
     }
 }
