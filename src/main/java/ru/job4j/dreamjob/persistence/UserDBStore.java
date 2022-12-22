cd28e2ae3b98088400736d1f9f128703357350fc
Lenovo <vorota-24@bk.ru>
2022-12-22 12:36:31 +0300

    1. Страница login.html [#504863]

--- src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
+++ src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
@@ -10,6 +10,7 @@ import ru.job4j.dreamjob.model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Repository
@@ -18,6 +19,7 @@ public class UserDBStore {
     public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private final static String ADD = "insert into users(email, password) VALUES (?,?)";
     private final static String FIND_BY_ID = "SELECT * FROM candidate WHERE id = ?";
+    private final static String FIND_BY_USER_PASS = "SELECT * from  users WHERE email like ? and password like ?";
 
     private final BasicDataSource pool;
 
@@ -52,14 +54,35 @@ public class UserDBStore {
             ps.setInt(1, id);
             try (ResultSet it = ps.executeQuery()) {
                 if (it.next()) {
-                    return Optional.of(new User(it.getInt("id"),
-                            it.getString("email"), it.getString("password")));
+                    return Optional.of(createUser(it));
                 }
             }
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
         return Optional.empty();
+    }
+
+    public Optional<User> findUserByEmailAndPassword(String email, String password) {
+        try (Connection cn = pool.getConnection();
+        PreparedStatement ps = cn.prepareStatement(FIND_BY_USER_PASS)) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return Optional.of(createUser(it));
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return Optional.empty();
+    }
+
+    public User createUser(ResultSet it) throws SQLException {
+        return new User(it.getInt("id"),
+                it.getString("email"),
+                it.getString("password"));
     }
 
 }
