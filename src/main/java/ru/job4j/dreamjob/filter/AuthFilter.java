7d8cbb779d1a0f499eae42d7d47faf7cc4f5a77a
Lenovo <vorota-24@bk.ru>
2023-01-01 14:12:32 +0300

    3. Filter [#504865 #376449]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -18,7 +18,8 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (uri.endsWith("loginPage") || uri.endsWith("login")
+                        || uri.endsWith("registration") || uri.endsWith("formRegistration")) {
             chain.doFilter(req, res);
             return;
         }
